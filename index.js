2f19916807a25646a2034b5f0d146eeebc69c70e
anonimuslim <kevinkrisna27@gmail.com>
2023-06-08 01:02:36 +0700

    Update setTimeout at function(error)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $.ajax({
     url: "https://pokeapi.co/api/v2/pokemon",
     type: "GET", // tidak wajib karena default=GET
-    success: function(respons){
+    success: function(response){
         let text = "";
-        $.each(respons.results, function(key, value){
+        $.each(response.results, function(key,value){
             text += `
             <tr>
                 <td>${key+1}</td>
                 <td class="text-capitalize">${value.name}</td>
-                <td>${value.gender}</td>
+                <td>${value.url}</td>
                 <td>
                     <button
                     type="button"
@@ -25,22 +25,22 @@ $.ajax({
         });
         $("#tbodySW").html(text);
     },
-    error: setTimeout(
-        function(error){
-        alert("Maaf, terjadi kesalahan dalam memuat data. Silakan coba lagi nanti.");
-        console.log("Terjadi kesalahan:", error);
-        },0) // dikasih setTimeout 0 agar alert tidak delay
+    error: function(error){
+        setTimeout(function(){
+            alert("Maaf, terjadi kesalahan dalam memuat data. Silakan coba lagi nanti.");
+            console.log("Terjadi kesalahan:", error);
+        }, 0); // dikasih setTimeout 0 agar alert tidak delay
+    }
 })
 
 function detail(value) {
     // console.log(val);
-  
     $.ajax({
       url: value,
-      success: function (respons) { 
-        array.forEach(element => {
-            $("#bodySW").html(respons.abilities[element].ability.name); 
-        });
+      success: function (response) { 
+        $.each(response.abilities, function(key,value){
+            $("#bodySW").html(value.ability.name); 
+        })
       },
     });
   }
