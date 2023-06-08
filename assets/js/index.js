aa95b6ab795d0ec4e886757a557ce4a821d17a9b
anonimuslim <kevinkrisna27@gmail.com>
2023-06-08 23:22:10 +0700

    Adding animations using the hover.css library

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ $.ajax({
                   <td>${data.height}</td>
                   <td>${data.weight}</td>
                   <td>
-                      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="detail('${value.url}')">Detail</button>
+                      <button type="button" class="btn btn-primary hvr-buzz" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="detail('${value.url}')">Detail</button>
                   </td>
               </tr>
           `;
@@ -55,57 +55,54 @@ function detail(value) {
     url: value,
     success: function (response) {
       let statsTitle = ["HP","Attack","Defense","Special Attack","Special Defense","Speed"];
+      /*
+      HP: danger -> merah
+      Attact: warning -> kuning
+      Defense: info -> biru muda
+      Special Attack: warning-emphasis -> kuning muda
+      Special Defence: info-emphasis -> biru muda
+      Speed: success -> hijau
+      */
+      let progressColor = ["bg-danger","bg-warning","bg-info","bg-warning","bg-info","bg-success"];
       let details = "";
-      let ability = "";
 
       details = `
       <div id="modalBody" class="row me-2 ms-2 d-flex align-items-center">
-          <p id="pokeName" class="text-center badge bg-warning">${response.forms[0].name}</p>
+          <p id="pokeName" class="text-center badge bg-warning hvr-wobble-bottom">${response.forms[0].name}</p>
           <div class="col-6 text-center">
-            <img src='${response.sprites.other.dream_world.front_default}' title='Gambar Pokemon ${response.forms[0].name}' alt='Gambar Pokemon ${response.forms[0].name}'>
-            <p><span class="badge bg-success mt-4 text-center">Kenalin, dia ${response.forms[0].name}, comel kan? <img src="/assets/images/laughing.png" width=17/></span></p>
+            <img src='${response.sprites.other.dream_world.front_default}' title='Gambar Pokemon ${response.forms[0].name}' alt='Gambar Pokemon ${response.forms[0].name}' class="hvr-grow">
+            <p><span class="badge bg-success mt-4 text-center hvr-wobble-horizontal">Kenalin, dia ${response.forms[0].name}, comel kan? <img src="/assets/images/laughing.png" width=17/></span></p>
           </div>
 
           <div class="col-6">
             <div class="row border rounded-4">
-              <div id="statsTitle" class="col mt-4">
-              ${(() => {
-                let titles = '';
-                for (let index = 0; index < statsTitle.length; index++) {
-                  titles += `<p>${statsTitle[index]}</p>`;
-                }
-                return titles;
-              })()}
+              <div class="col mt-4">
+                ${(() => {
+                  let titles = '';
+                  for (let index = 0; index < statsTitle.length; index++) {
+                    titles += `<p><span class="hvr-bounce-in">${statsTitle[index]}</span></p>`;
+                  }
+                  return titles;
+                })()}
               </div>
               <div class="col mt-4">
-                <div class="progress" role="progressbar" aria-label="${response.stats[0].stat.name}" aria-valuenow="${response.stats[0].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: ${response.stats[0].base_stat}%">${response.stats[0].base_stat}%</div>
-                </div>
-                <br>
-                <div class="progress" role="progressbar" aria-label="${response.stats[1].stat.name}" aria-valuenow="${response.stats[1].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width: ${response.stats[1].base_stat}%">${response.stats[1].base_stat}</div>
-                </div>
-                <br>
-                <div class="progress" role="progressbar" aria-label="${response.stats[2].stat.name}" aria-valuenow="${response.stats[2].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: ${response.stats[2].base_stat}%">${response.stats[2].base_stat}</div>
-                </div>
-                <br>
-                <div class="progress" role="progressbar" aria-label="${response.stats[3].stat.name}" aria-valuenow="${response.stats[3].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: ${response.stats[3].base_stat}%">${response.stats[3].base_stat}</div>
-                </div>
-                <br>
-                <div class="progress" role="progressbar" aria-label="${response.stats[4].stat.name}" aria-valuenow="${response.stats[4].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: ${response.stats[4].base_stat}%">${response.stats[4].base_stat}</div>
-                </div>
-                <br>
-                <div class="progress" role="progressbar" aria-label="${response.stats[5].stat.name}" aria-valuenow="${response.stats[4].base_stat}" aria-valuemin="0" aria-valuemax="100">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: ${response.stats[5].base_stat}%">${response.stats[5].base_stat}</div>
-                </div>
+                ${(() => {
+                  let progressBar = '';
+                  for (let index = 0; index < progressColor.length; index++) {
+                    progressBar += `
+                      <div class="progress" role="progressbar" aria-label="${response.stats[index].stat.name}" aria-valuenow="${response.stats[index].base_stat}" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar progress-bar-striped progress-bar-animated ${progressColor[index]} hvr-pulse-grow" style="width: ${response.stats[index].base_stat}%">${response.stats[index].base_stat}%</div>
+                      </div>
+                      <br>
+                    `;
+                  }
+                  return progressBar;
+                })()}
               </div>
             </div>
-            <hr>
-        <p>Hello world!</p>
           </div>
+          <hr class="lineCustom">
+          <p>Hello world!</p>
         </div>
       `;
       
@@ -125,7 +122,6 @@ function detail(value) {
 
 /* Untuk looping progress bar, tapi masih perlu diperbaiki
 ${(() => {
-  let progressColor = ["bg-info","bg-danger","bg-warning","bg-success","bg-dark"];
   for (let index = 0; index < progressColor.length; index++) {
     titles += `<p>${stats[index]}</p>`;
   }
@@ -134,4 +130,30 @@ ${(() => {
 
 Kali ini untuk statsTitlenya: tidak dipakai karena seperti ada margin top yang menyebabkan tidak sejajar dengan progress bar
 <p>${statsTitle.map(title => `<p>${title}</p>`).join('')}</p>
+
+
+Dibuang sayang:
+<div class="progress" role="progressbar" aria-label="${response.stats[0].stat.name}" aria-valuenow="${response.stats[0].base_stat}" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped progress-bar-animated hvr-pulse-grow" style="width: ${response.stats[0].base_stat}%">${response.stats[0].base_stat}%</div>
+</div>
+<br>
+<div class="progress" role="progressbar" aria-label="${response.stats[1].stat.name}" aria-valuenow="${response.stats[1].base_stat}" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width: ${response.stats[1].base_stat}%">${response.stats[1].base_stat}</div>
+</div>
+<br>
+<div class="progress" role="progressbar" aria-label="${response.stats[2].stat.name}" aria-valuenow="${response.stats[2].base_stat}" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: ${response.stats[2].base_stat}%">${response.stats[2].base_stat}</div>
+</div>
+<br>
+<div class="progress" role="progressbar" aria-label="${response.stats[3].stat.name}" aria-valuenow="${response.stats[3].base_stat}" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: ${response.stats[3].base_stat}%">${response.stats[3].base_stat}</div>
+</div>
+<br>
+<div class="progress" role="progressbar" aria-label="${response.stats[4].stat.name}" aria-valuenow="${response.stats[4].base_stat}" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: ${response.stats[4].base_stat}%">${response.stats[4].base_stat}</div>
+</div>
+<br>
+<div class="progress" role="progressbar" aria-label="${response.stats[5].stat.name}" aria-valuenow="${response.stats[4].base_stat}" aria-valuemin="0" aria-valuemax="100">
+  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: ${response.stats[5].base_stat}%">${response.stats[5].base_stat}</div>
+</div>
 */
